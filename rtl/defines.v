3d9d1992216b87d970f5c9dc76be9201da4d2deb
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-05-24 12:47:27 -0300

    (defines.v) usando defines.v para globais

---
+++ rtl/defines.v
@@ -0,0 +1,13 @@
+`ifndef defines
+`define defines
+  
+`define OP_ADD 4'h0
+`define OP_SUB 4'h1
+`define OP_SHR 4'h2
+`define OP_SHL 4'h3
+`define OP_OR  4'h4
+`define OP_AND 4'h5
+`define OP_XOR 4'h6
+`define OP_PASS 4'hf
+  
+`endif
--- rtl/ula.sv
+++ rtl/ula.sv
@@ -1,21 +1,7 @@
+`include "defines.v"
 /**
 * 16-bit Logic Arithmetic Unit
 */
-parameter
-  // Arith
-  OP_ADD = 4'h0,
-  OP_SUB = 4'h1,
-  
-  // Logic
-  OP_SHR = 4'h2,
-  OP_SHL = 4'h3,
-  OP_OR  = 4'h4,
-  OP_AND = 4'h5
-  OP_XOR = 4'h6,
-
-  // OUT = B
-  OP_PASS = 4'hf;
-
 module ALU_16 (
   input [15:0] a, b;
   output logic [15:0] out;
@@ -25,4 +11,4 @@ module ALU_16 (
   logic z, c, v, n;
   assign flags = {z, c, v, n};
 
-endmodule;
+endmodule
