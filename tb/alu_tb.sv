fe5542b51944f6014e4a2e080abe1f9ebc9ffcb5
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-05-30 10:14:27 -0300

    (micro16/ula) melhorando testbench um pouco

--- tb/alu_tb.sv
+++ tb/alu_tb.sv
@@ -1,8 +1,65 @@
+`include "defines.v"
+
 module alu_tb;
 
-initial begin 
-  $display("Iniciando simulação da ULA 16 bits");
-  $finish;
-end 
+  logic [3:0] operation;
+  logic [15:0] a;
+  logic [15:0] b;
+  logic [15:0] out;
+  logic [3:0] flags;
+
+  logic z, c, v, n;
+  assign {z, c, v, n} = flags;
+
+  alu dut ( .operation(operation), .a(a), .b(b), .out(out), .flags(flags) );
+
+  task check_result(
+    input logic [15:0] a_in,
+    input logic [15:0] b_in,
+    input logic [3:0] op_in,
+    input logic [15:0] expected
+  );
+  begin
+    a  = a_in;
+    b  = b_in;
+    operation = op_in;
+
+    #1;
+
+    if (out !== expected)
+      $error(
+        "FAIL op=%0d a=%0d b=%0d expected=%0d got=%0d",
+         op_in, a_in, b_in, expected, out
+      );
+
+    if (z != (out == 0))
+      $error("FAIL flag Z not set when output 0");
+
+    if (n != (out < 0))
+      $error("FAIL flag N not set when out < 0");
+
+    if (op_in == `OP_ADD) begin
+      if (v != ((a < 0 && b < 0 && out > 0) 
+        || (a > 0 && b > 0 && out < 0)))
+        $error("FAIL flag V not set when overflow");
+
+      if (c != (a + b > 255))
+        $error("FAIL incorrect carry flag");
+    end 
+
+  end
+  endtask
+
+
+  initial begin 
+    $display("--- Starting ALU simulation ---");
+    $display("  A    B    Out   ZCVN ");
+    $monitor(" %3d %3d %4d %b ", a, b, out, flags);
+
+    check_result(10, 20, `OP_ADD, 30);
+    check_result(0, 0, `OP_ADD, 0);
+
+    $finish;
+  end 
 
 endmodule;
