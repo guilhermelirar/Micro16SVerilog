0e02a5b04c41a1f54083103d0d443dfd099d3ae2
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-06-07 17:27:54 -0300

    (alu) testing alu v2

--- tb/alu_tb.sv
+++ tb/alu_tb.sv
@@ -13,51 +13,24 @@ module alu_tb;
 
   alu dut ( .operation(operation), .a(a), .b(b), .out(out), .flags(flags) );
 
-  task check_result(
-    input logic [15:0] a_in,
-    input logic [15:0] b_in,
-    input logic [3:0] op_in,
-    input logic [15:0] expected
-  );
-  begin
-    a  = a_in;
-    b  = b_in;
-    operation = op_in;
-
+  always @* begin
     #1;
+    if (operation == `OP_ADD) begin
+      asm_sum: assert ({c, out} == a + b);
+    end
 
-    if (out !== expected)
-      $error(
-        "FAIL op=%0d a=%0d b=%0d expected=%0d got=%0d",
-         op_in, a_in, b_in, expected, out
-      );
-
-    if (z != (out == 0))
-      $error("FAIL flag Z");
-
-    if (n != out[15])
-      $error("FAIL flag N");
-
-    if (op_in == `OP_ADD) begin
-      if (v != ((~a[15] && ~b[15] && out[15]) 
-        || (a[15] && b[15] && ~out[15])))
-        $error("FAIL flag V");
-
-      if (c != (({1'b0, a} + {1'b0, b}) >> 16))
-        $error("FAIL incorrect carry flag");
-    end 
-
+    if (out == 0)
+      asm_zero_1: assert (z == 1'b1);
+    else
+      asm_zero_0: assert (z == 1'b0);
+  
   end
-  endtask
 
   task test_random;
-    logic [15:0] a_in;
-    logic [15:0] b_in;
-
     for (int i = 0; i < 100; i++) begin
-      a_in = $random;
-      b_in = $random;
-      check_result(a_in, b_in, `OP_ADD, a_in + b_in);     
+      a = $random;
+      b = $random;
+      operation <= `OP_ADD;
     end 
   endtask
 
@@ -66,14 +39,16 @@ module alu_tb;
     $display("  A     B     Out    ZCVN ");
     $monitor(" %-5d  %-5d %-5d  %b ", a, b, out, flags);
 
-    check_result(10, 20, `OP_ADD, 30);
-    check_result(0, 0, `OP_ADD, 0);
-    check_result(16'hffff, 1, `OP_ADD, 0);
-    check_result(10, -20, `OP_ADD, -10);
-    check_result(16'h7fff, 1, `OP_ADD, 16'h8000);
-    test_random();
+    forever #1 test_random();
+  end
 
-    $finish;
+  initial begin 
+    $display("--- Starting ALU simulation ---");
+    $display("  A     B     Out    ZCVN ");
+    $monitor(" %-5d  %-5d %-5d  %b ", a, b, out, flags);
+
+
+    #200 $finish;
   end 
 
 endmodule;
