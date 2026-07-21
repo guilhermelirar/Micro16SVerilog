d4352676a5bbdc8c2ce8c5415491c6ebcb776a8b
Guilherme Lira Ribeiro <gui.liraribeiro10@gmail.com>
2026-07-20 21:09:05 -0300

    (refactor alu.sv) more explicit shift with 4 less significative bits

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -4,7 +4,7 @@
 */
 module alu (
   input [3:0] operation,
-  input [15:0] a, 
+  input [15:0] a,
   input [15:0] b,
   output logic [15:0] out,
   output logic [3:0] flags // Z, C, V, N
@@ -25,40 +25,40 @@ module alu (
 
       `OP_SUB: begin
         out = a - b;
-        v = (~a[15] & b[15] & n) | 
-          (a[15] & ~b[15] & ~n); 
-      end 
+        v = (~a[15] & b[15] & n) |
+          (a[15] & ~b[15] & ~n);
+      end
 
-      `OP_SHR: begin 
-        out = a >> b & 4'hF;
+      `OP_SHR: begin
+        out = a >> b[3:0];
         v = 0; c = 0; // TODO
-      end 
+      end
 
-      `OP_SHL: begin 
-        out = a << b & 4'hF;
+      `OP_SHL: begin
+        out = a << b[3:0];
         v = 0; c = 0; // TODO
-      end 
+      end
 
-      `OP_OR: begin 
+      `OP_OR: begin
         out = a | b;
         v = 0; c = 0;
       end
 
-      `OP_AND: begin 
+      `OP_AND: begin
         out = a & b;
         v = 0; c = 0;
       end
 
-      `OP_XOR: begin 
+      `OP_XOR: begin
         out = a ^ b;
         v = 0; c = 0;
       end
 
-      `OP_PASS: begin 
+      `OP_PASS: begin
         out = b;
         v = 0; c = 0;
-      end 
-    endcase 
-  end 
+      end
+    endcase
+  end
 
 endmodule
